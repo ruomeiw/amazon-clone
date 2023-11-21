354bf2d7d9f5cc7746d6991b569f706375aa7ff2
Della Wang <ruomeiw@andrew.cmu.edu>
2023-11-21 16:41:13 -0500

    Add project files

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// import firebase from 'firebase/compat/app';
+// import 'firebase/compat/auth';
+// import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDsyQxLOTy0iZHOBIaUu10jNHHoBjAZLHU",
+  authDomain: "clone-fd9ca.firebaseapp.com",
+  projectId: "clone-fd9ca",
+  storageBucket: "clone-fd9ca.appspot.com",
+  messagingSenderId: "544477990230",
+  appId: "1:544477990230:web:15d9afeffae8f832c21af7",
+  measurementId: "G-D408SF5XD2"
+};
+
+// const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+// const db = firebaseApp.firestore();
+// const auth = firebase.auth();
+
+// export { db, auth };
